946f047ed94b188f2846c59fdea1a6ed53dfae12
RijanPrajapati1 <122766161+RijanPrajapati1@users.noreply.github.com>
2025-07-30 12:27:23 +0545

    axios frontend fixing

--- Wheeloop-Web-FrontEnd/src/components/utils/axios.jsx
+++ Wheeloop-Web-FrontEnd/src/components/utils/axios.jsx
@@ -1,14 +1,11 @@
-// src/utils/axios.js
-
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    // FIX: Change the baseURL to just the domain and port
     baseURL: 'https://localhost:3001/api',
     timeout: 5000,
 });
 
-// Add a "request interceptor"
+
 axiosInstance.interceptors.request.use(
     (config) => {
         const token = localStorage.getItem('authToken');
